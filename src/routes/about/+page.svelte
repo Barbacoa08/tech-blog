16687230280496b7540298645df2db6f9d992989
barbajoe <sabe12070@gmail.com>
2023-12-21 15:57:36 -0600

    v1, add About page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,29 @@
-<h1>about</h1>
+<svelte:head>
+  <title>Blog: About</title>
+  <meta name="description" content="barbajoe's Tech Blog" />
+</svelte:head>
+
+<h1>About</h1>
+
+<p>Hello there 👋</p>
+
+<p>
+  The "barbajoe" link in the site footer will take you to a list of my recent projects, or you can
+  view my <a href="https://rxresu.me/barbajoe/joe-sebast-2024">resume</a> for more details about me and
+  what I do, but the short version is that I'm a full stack engineer with a front-end focus.
+</p>
+
+<p>
+  I am currently a tech lead and am slowly dipping my toes into an architecture role as my manager
+  is trying to get me to stretch myself into a higher-level role, which is fun! A bit daunting, but
+  there are a lot of great resources out there. Really quickly, Mark Richards <a
+    href="https://www.youtube.com/@markrichards5014">YouTube channel</a
+  > is excellent. Out of all the resources that I've been exploring, including paid options, his Software
+  Architecture Monday series is my favorite.
+</p>
+
+<p>
+  I'll be using this blog as a journal. I will document what I've found that I want to remember and
+  what I've accomplished. I hope that you find it interesting, easy, and helpful. With that said,
+  let's get good!
+</p>
