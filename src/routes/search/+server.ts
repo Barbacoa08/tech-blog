d9572babcdf3fb76829ff748eee958303bb3d4ce
barbajoe <sabe12070@gmail.com>
2023-12-21 10:36:03 -0600

    fix global search

--- src/routes/search/+server.ts
+++ src/routes/search/+server.ts
@@ -2,12 +2,44 @@ import { json } from "@sveltejs/kit";
 
 import type { RequestHandler } from "./$types";
 
+import { getPosts } from "$lib/server/posts";
+import type { Post } from "$types/payload-types";
 import type { TermsResult } from "$types/SearchAPI";
 
+/**
+ * This call takes ~10ms
+ *
+ * It's not really necessary to make this its own function. This could easily
+ * live in the `load` function, but I want to play with a `+server.ts` file and
+ * see how it works.
+ *
+ * @returns {Promise<TermsResult[]>}
+ */
 export const GET = (async () => {
-  return json([
+  let posts: Post[] = [];
+
+  try {
+    posts = await getPosts(fetch);
+  } catch (error) {
+    console.error(error);
+  }
+
+  const results: TermsResult[] = [
     { tags: ["homepage", "root"], title: "Home", url: "/" },
     { tags: ["posts root"], title: "Posts", url: "/post" },
     { tags: ["why this exists"], title: "About", url: "/about" },
-  ] as TermsResult[]);
+  ] as TermsResult[];
+  posts.forEach((post) => {
+    const { title, slug, tags, status, publishedDate } = post;
+
+    if (status === "published" && publishedDate && new Date(publishedDate) < new Date() && title) {
+      results.push({
+        title,
+        url: `/post/${slug}`,
+        tags: tags?.map((tag) => tag.name || "") || [],
+      });
+    }
+  });
+
+  return json(results);
 }) satisfies RequestHandler;
