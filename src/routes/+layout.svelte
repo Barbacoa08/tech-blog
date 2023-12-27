d414655a72d8c0dcca24d2206c3bccdbc81e51fb
Joe Sebast <sabe12070@gmail.com>
2023-12-27 13:00:46 -0600

    add social icons to footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { Layout } from "@barbajoe/svelte-lib";
+  import { Icons, Layout, Link } from "@barbajoe/svelte-lib";
   import { SvelteToast } from "@zerodevx/svelte-toast";
 
   import { Header } from "$lib/components";
@@ -15,6 +15,36 @@
   <Header slot="header" />
 
   <slot />
+
+  <svelte:fragment slot="footer">
+    &forall; by <Link isExternal href="https://barbajoe.tech/">
+      <code>barbajoe</code>
+    </Link>
+
+    <span class="social-icons">
+      <Link isExternal href="https://bsky.app/profile/barbajoe.tech" class="link">
+        <Icons.Bluesky height="0.7rem" width="0.7rem" />
+      </Link>
+      |
+      <Link isExternal href="https://www.linkedin.com/in/joseph-sebast/" class="link">
+        <Icons.LinkedIn height="0.7rem" width="0.7rem" />
+      </Link>
+    </span>
+  </svelte:fragment>
 </Layout>
 
 <SvelteToast {options} />
+
+<style>
+  .social-icons {
+    padding-left: 0.1rem;
+
+    & .link {
+      padding: 0 0.5rem;
+
+      &:last-child {
+        padding-right: 0;
+      }
+    }
+  }
+</style>
