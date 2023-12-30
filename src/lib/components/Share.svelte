d0fac72d2185ad10e04b1164fb4773a97cc658a9
barbajoe <sabe12070@gmail.com>
2023-12-30 14:47:27 -0600

    fix Share button

--- src/lib/components/Share.svelte
+++ src/lib/components/Share.svelte
@@ -4,8 +4,14 @@
 
   export let text: string;
   export let url: string;
+
+  const callback = ({ webshared }: { webshared: boolean }) => {
+    if (webshared) {
+      toast.push("Shared!");
+    } else {
+      toast.push("Copied URL to clipboard");
+    }
+  };
 </script>
 
-<Share title="barbajoe's blog" {text} {url} callback={() => toast.push("Copied URL to clipboard")}>
-  Share
-</Share>
+<Share title="barbajoe's blog" {text} {url} {callback}>Share</Share>
