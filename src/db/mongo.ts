f6e9d124beb57dee497865b2d9ac458d21fd6615
barbajoe <sabe12070@gmail.com>
2024-02-24 16:51:11 -0600

    update per coderabbit suggestion

--- src/db/mongo.ts
+++ src/db/mongo.ts
@@ -3,5 +3,6 @@ import { MongoClient } from "mongodb";
 
 const client = new MongoClient(MONGODB_URI);
 
-export const start_mongo = () => client.connect();
+export const start_mongo = () =>
+  client.connect().catch((err) => console.error("MongoDB connection error:", err));
 export const blogDb = client.db("blog");
